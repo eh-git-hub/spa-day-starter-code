7390fa331e859f88c0dd59b324447f7e1993ef3b
Edgar Hernandez <EdgarHomeFolder@edgars-mbp.attlocal.net>
2021-02-22 21:24:17 -0600

    created new template for customerForm and added css for footer

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -44,26 +44,8 @@ public class SpaDayController {
     }
 
     @GetMapping(value="")
-    @ResponseBody
     public String customerForm () {
-        String html = "<form method = 'post'>" +
-                "Name: <br>" +
-                "<input type = 'text' name = 'name'>" +
-                "<br>Skin type: <br>" +
-                "<select name = 'skintype'>" +
-                "<option value = 'oily'>Oily</option>" +
-                "<option value = 'combination'>Combination</option>" +
-                "<option value = 'normal'>Normal</option>" +
-                "<option value = 'dry'>Dry</option>" +
-                "</select><br>" +
-                "Manicure or Pedicure? <br>" +
-                "<select name = 'manipedi'>" +
-                "<option value = 'manicure'>Manicure</option>" +
-                "<option value = 'pedicure'>Pedicure</option>" +
-                "</select><br>" +
-                "<input type = 'submit' value = 'Submit'>" +
-                "</form>";
-        return html;
+        return "customerForm";
     }
 
     @PostMapping(value="")
